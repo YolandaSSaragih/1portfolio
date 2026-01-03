08ab80843ff88a2dc2e639c7ff8259640a32dc9b
YolandaSSaragih <yolandasaragih69@gmail.com>
2026-01-03 12:18:41 +0700

    gfe

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/1portfolio">
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
